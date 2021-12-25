5ca61b51f115e6b240561c8e0413affa3fd2660c
s-raza+S20-Win <12784652+S20-Win+s-raza@users.noreply.github.com>
2021-12-25 18:42:27 +0400

    Disable default singnup, login viewsets

--- home/api/v1/urls.py
+++ home/api/v1/urls.py
@@ -2,14 +2,14 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .viewsets import AppViewSet, PlanViewSet, SubscriptionViewSet
 
-from home.api.v1.viewsets import (
-    SignupViewSet,
-    LoginViewSet,
-)
+# from home.api.v1.viewsets import (
+#     SignupViewSet,
+#     LoginViewSet,
+# )
 
 router = DefaultRouter()
-router.register("signup", SignupViewSet, basename="signup")
-router.register("login", LoginViewSet, basename="login")
+# router.register("signup", SignupViewSet, basename="signup")
+# router.register("login", LoginViewSet, basename="login")
 router.register("apps", AppViewSet)
 router.register("subscriptions", SubscriptionViewSet)
 router.register("plans", PlanViewSet)
--- home/api/v1/viewsets.py
+++ home/api/v1/viewsets.py
@@ -7,31 +7,31 @@ from rest_framework.viewsets import ModelViewSet, ViewSet
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 
-from home.api.v1.serializers import (
-    SignupSerializer,
-    UserSerializer,
-)
+# from home.api.v1.serializers import (
+#     SignupSerializer,
+#     UserSerializer,
+# )
 
 
-class SignupViewSet(ModelViewSet):
-    serializer_class = SignupSerializer
-    http_method_names = ["post"]
+# class SignupViewSet(ModelViewSet):
+#     serializer_class = SignupSerializer
+#     http_method_names = ["post"]
 
 
-class LoginViewSet(ViewSet):
-    """Based on rest_framework.authtoken.views.ObtainAuthToken"""
+# class LoginViewSet(ViewSet):
+#     """Based on rest_framework.authtoken.views.ObtainAuthToken"""
 
-    serializer_class = AuthTokenSerializer
+#     serializer_class = AuthTokenSerializer
 
-    def create(self, request):
-        serializer = self.serializer_class(
-            data=request.data, context={"request": request}
-        )
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data["user"]
-        token, created = Token.objects.get_or_create(user=user)
-        user_serializer = UserSerializer(user)
-        return Response({"token": token.key, "user": user_serializer.data})
+#     def create(self, request):
+#         serializer = self.serializer_class(
+#             data=request.data, context={"request": request}
+#         )
+#         serializer.is_valid(raise_exception=True)
+#         user = serializer.validated_data["user"]
+#         token, created = Token.objects.get_or_create(user=user)
+#         user_serializer = UserSerializer(user)
+#         return Response({"token": token.key, "user": user_serializer.data})
 
 
 class AppViewSet(viewsets.ModelViewSet):
@@ -44,12 +44,12 @@ class AppViewSet(viewsets.ModelViewSet):
 
 
 class SubscriptionViewSet(
-        mixins.CreateModelMixin, 
-        mixins.RetrieveModelMixin, 
-        mixins.UpdateModelMixin,
-        mixins.ListModelMixin,
-        viewsets.GenericViewSet
-    ):
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet
+):
     serializer_class = SubscriptionSerializer
     authentication_classes = (
         authentication.SessionAuthentication,
