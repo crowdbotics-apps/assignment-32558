cc90e68f97532da6b0696321db038392c13879ae
s-raza+S20-Win <12784652+S20-Win+s-raza@users.noreply.github.com>
2021-12-25 22:53:30 +0400

    Update model names in view

--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from .models import App, Plans, Subscriptions
+from .models import App, Plan, Subscription
 from django.views.generic import CreateView, UpdateView, DetailView, ListView
 from django.shortcuts import render
 
