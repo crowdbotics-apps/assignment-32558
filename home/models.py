1bf8fd5daf83c293f38529d33c95af1aaf79fcb9
s-raza+S20-Win <12784652+S20-Win+s-raza@users.noreply.github.com>
2021-12-25 14:18:21 +0400

    Update model: Plan

--- home/models.py
+++ home/models.py
@@ -92,12 +92,11 @@ class Plan(models.Model):
     name = models.CharField(
         max_length=20,
     )
-    description = models.CharField(
-        max_length=256,
-    )
+    description = models.TextField()
     price = models.DecimalField(
         max_digits=3,
         decimal_places=2,
+        blank=True
     )
     created_at = models.DateTimeField(
         auto_now_add=True,
