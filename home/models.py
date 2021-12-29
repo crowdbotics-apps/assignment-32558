0861daf8672a7bc8e3b81b781318a7d46c101797
rs-2019 <96575616+rs-2019@users.noreply.github.com>
2021-12-29 11:50:17 +0400

    Update models: add __str__

--- home/models.py
+++ home/models.py
@@ -54,6 +54,9 @@ class App(models.Model):
     updated_at = models.DateTimeField(
         auto_now=True,
     )
+    
+    def __str__(self):
+        return self.name
 
 
 class Subscription(models.Model):
@@ -96,7 +99,7 @@ class Plan(models.Model):
     )
     description = models.TextField()
     price = models.DecimalField(
-        max_digits=3,
+        max_digits=4,
         decimal_places=2,
         blank=True
     )
@@ -106,3 +109,6 @@ class Plan(models.Model):
     updated_at = models.DateTimeField(
         auto_now=True,
     )
+
+    def __str__(self):
+        return f"{self.name} ({self.price})"
